2bd5987b898d70de6a5bae0028d7841be7d74db8
Yaron Golan <yargolan@gmail.com>
2022-09-02 17:01:39 +0300

    [Diff] - Rewrite the code, more efficient

--- Scripts/find_duplicated_files.py
+++ Scripts/find_duplicated_files.py
@@ -4,69 +4,26 @@ import sys
 import hashlib
 
 
+data = {}
+
 
 def calculate_checksum(filename, block_size=65536):
-
     md5_hash = hashlib.md5()
-
     with open(filename, "rb") as file:
         for block in iter(lambda: file.read(block_size), b""):
             md5_hash.update(block)
     return md5_hash.hexdigest()
 
 
-
-def print_duplications(data):
-
-    for entry in data:
-
-        if len(data[entry]) > 1:
-            for place in data[entry]:
-                print(place)
-
-            print("Checksum", entry)
-            print("")
-
-
-
-def get_root_dir(arguments):
-
-    # Check if there is a root dir as a parameter
-    if len(arguments) == 2:
-        root_dir = arguments[1]
-    else:
-        root_dir = input("Insert the root folder: ")
-
-    return root_dir
-
-
-
-def main(args):
-
-    # Get the root dir
-    root_dir = get_root_dir(args)
-
+def main(root_dir):
 
     # Validate the root dir
     if not os.path.isdir(root_dir):
         sys.exit(f"The given root folder ({root_dir}) is invalid.")
 
-
-    # Read the files recursively and check for duplication
-    duplications = search_for_duplications(starting_point=root_dir)
-
-
-    # Print the duplicated files.
-    print_duplications(duplications)
-
-
-
-def search_for_duplications(starting_point):
-
-    data = {}
-
     # r=root, d=directories, f = files
-    for r, d, f in os.walk(starting_point):
+    print("Searching for files...")
+    for r, d, f in os.walk(root_dir, followlinks=False):
         for file in f:
             current_file = os.path.join(r, file)
 
@@ -74,6 +31,7 @@ def search_for_duplications(starting_point):
             if os.path.isdir(current_file):
                 continue
 
+            print(".", end="")
             file_checksum = calculate_checksum(current_file)
 
             if file_checksum in data:
@@ -82,10 +40,25 @@ def search_for_duplications(starting_point):
             else:
                 places = [current_file]
                 data[file_checksum] = places
+    print()
 
-    return data
 
+def print_report():
+    duplications = {}
+    for item in data:
+        if len(data[item]) > 1:
+            duplications[item] = data[item]
+
+    if duplications:
+        for item in duplications:
+            print(f"{item} : {duplications[item]}")
+    else:
+        print("No duplications found.")
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+        print_report()
+    else:
+        print("Usage:\n" + sys.argv[0] + "  <Root dir>")
